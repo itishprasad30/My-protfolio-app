0b9b062edd4f44a9195d44043bfdb049f06a9305
itishprasad30 <itishprasad20@gmail.com>
2021-09-09 20:51:43 +0530

    Inital with next js

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import "tailwindcss/tailwind.css";
+import { ThemeProvider } from "next-themes";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider defaultTheme="light" attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
