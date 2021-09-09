f3b25bd62764fa592237325732485aa915c6fc70
itishprasad30 <itishprasad20@gmail.com>
2021-09-09 20:40:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+The website is built in my favourite Tech Stack and deployed on Vercel
+
+👉Next.js for building React Components.
+👉tailwindcss for styling.
+👉React Rough Notation for Hero section highlighting.
+👉Vercel for deployments.
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      transitionDuration: {
+        0: "0ms",
+        2000: "2000ms",
+        3000: "3000ms",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
