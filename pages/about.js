cb8cfc94c081b0f0e351a16e08dcfb9625052cf4
itishprasad30 <itishprasad20@gmail.com>
2021-09-09 20:48:21 +0530

    Add Contact form, About section complete

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AboutMe from "../components/AboutMe";
+import ContainerBlock from "../components/ContainerBlock";
+
+function about() {
+  return (
+    <ContainerBlock>
+      <AboutMe />
+    </ContainerBlock>
+  );
+}
+
+export default about;
