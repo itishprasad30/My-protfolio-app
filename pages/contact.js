74a5baab8bad0d1364af7205fd2addc929394f5f
itishprasad30 <itishprasad20@gmail.com>
2021-09-09 20:50:25 +0530

    add contact page

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Contact from "../components/Contact";
+import ContainerBlock from "../components/ContainerBlock";
+
+function contact() {
+  return (
+    <ContainerBlock>
+      <Contact />
+    </ContainerBlock>
+  );
+}
+
+export default contact;
