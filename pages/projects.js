bf6f871a17176284d84a62cf70202b80cec4e69e
itishprasad30 <itishprasad20@gmail.com>
2021-09-09 20:50:46 +0530

    add project details to the page

---
+++ pages/projects.js
@@ -0,0 +1,12 @@
+import ContainerBlock from "../components/ContainerBlock";
+import Projects from "../components/Projects";
+
+function projects() {
+  return (
+    <ContainerBlock title="Projects - Itish Prasad">
+      <Projects />
+    </ContainerBlock>
+  );
+}
+
+export default projects;
